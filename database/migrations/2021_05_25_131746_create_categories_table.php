063655fbd73e069fb73381c279e47f359939196a
sekhar12322 <khanalsekhar13@gmail.com>
2021-05-25 19:13:16 +0545

    category migration and model created with controller

---
+++ database/migrations/2021_05_25_131746_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name');
+            $table->string('category_code');
+            $table->string('image')->nullable();
+            $table->longText('description')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->integer('parent_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
